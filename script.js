110c23c098dc8c8de0a61a29e5d9af84bfa4ef9a
daniel <daniel.silva.roch2@gmail.com>
2024-10-19 19:09:57 -0300

    Pokedex V1 added (1-151)

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-const pokeContainer = document.querySelector("#pokeContainer");
-
 const pokeCount = 151;
+
+const colors = {
+    fire: '#FDDFDF',
+    grass: '#DEFDE0',
+    electric: '#FCF7DE',
+    water: '#DEF3FD',
+    ground: '#f4e7da',
+    rock: '#d5d5d4',
+    fairy: '#fceaff',
+    poison: '#98d7a5',
+    bug: '#f8d5a3',
+    dragon: '#97b3e6',
+    psychic: '#eaeda1',
+    flying: '#F5F5F5',
+    fighting: '#E6E0D4',
+    normal: '#F5F5F5'
+}
+
+const mainTypes = Object.keys(colors);
 
 async function fetchPoke(id) {
     const url = "https://pokeapi.co/api/v2/pokemon";
@@ -9,9 +26,36 @@ async function fetchPoke(id) {
     return data;
 }
 
-async function getCard() {
+async function createCards(data) {
     for (let i = 1; i <= pokeCount; i++) {
+        const card = document.createElement('div');
+        card.classList.add("card");
         const data = await fetchPoke(i);
-        console.log(`Name: ${data.name}, Number: ${i}, Type: ${data.types[0].type.name}`);
+
+        const name = data.name[0].toUpperCase() + data.name.slice(1);
+        const id = data.id.toString().padStart(3, "0");
+
+        const types = data.types.map(type => type.type.name);
+        let type = `${mainTypes.find(type => types.indexOf(type) > -1)}`;
+        const color = colors[type];
+
+        card.style.backgroundColor = color;
+
+        const pokeInnerHTML = `
+            <div class="imgContainer">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png" alt="Name">
+            </div>
+
+            <div class="info">
+                <p class="number">#${id}</p>
+                <p class="name">${name}</p>
+                <p class="type">Type: ${type}</p>
+            </div>
+        `
+
+        card.innerHTML = pokeInnerHTML;
+        document.querySelector("#pokeContainer").appendChild(card);
     }
 }
+
+createCards()
